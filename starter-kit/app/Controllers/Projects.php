21dfa9a4bcfba5ebb619644876ba41e4ec63a5b7
nlim1978 <118269087+nlim1978@users.noreply.github.com>
2025-05-04 09:41:32 +0800

    ci4 plus shield ver 1.1
    My framework starter kit

---
+++ starter-kit/app/Controllers/Projects.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Projects extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
